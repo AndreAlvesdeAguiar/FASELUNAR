10a3f01d4c2cffacde5c86ee68381bbe10d2e4b0
AndreAlvesdeAguiar <aguiar1001@hotmail.com>
2023-06-11 00:46:06 -0300

    ajust

--- app/controllers/esp32_controller.rb
+++ app/controllers/esp32_controller.rb
@@ -7,25 +7,6 @@ class Esp32Controller < ApplicationController
       @dados2 = fetchSensorData("http://192.168.15.13/data")
     end
   
-    def stream
-      response.headers['Content-Type'] = 'text/event-stream'
-      10.times do
-        # Obtém os dados atualizados a cada iteração
-        @dados1 = fetchSensorData("http://192.168.15.12/data")
-        @dados2 = fetchSensorData("http://192.168.15.13/data")
-  
-        # Renderiza a página index.html.erb como uma string
-        rendered_html = render_to_string(template: 'esp32/index', layout: false)
-  
-        # Envia os dados atualizados para o cliente
-        response.stream.write("data: #{rendered_html}\n\n")
-  
-        sleep 1
-      end
-    ensure
-      response.stream.close
-    end
-  
     private
   
     def fetchSensorData(url)
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   root 'moon#index'
   get 'weather', to: 'weather#previsao', as: 'previsao'
   get 'sea', to: 'sea#previsao2', as: 'previsao2'
-  get 'stream', to: 'esp32#stream'
-  get 'esp32', to: 'esp32#index', as: 'index'
+  get 'esp32', to: 'esp32#index'
   
 end
